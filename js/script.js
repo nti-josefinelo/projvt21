f24d210f9eb0233786560d16dff95acd62d1483a
Josefine Lindbom Olsson <71005686+nti-josefinelo@users.noreply.github.com>
2021-03-25 17:44:47 +0100

    vecka 12

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // Denna fil skall innehålla JavaScript (JS)
+function meny() {
+    document.querySelector('.links').classList.toggle('navActive')
+    document.querySelector('.burger').classList.toggle('toggle')
+}
+
+document.querySelector('.burger').addEventListener('click', meny)
